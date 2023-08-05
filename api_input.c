97f582195a7b55d11b0c47ec28f987a64b2bfdf7
Lorenzo-Pigato <lorenzo.pigato@mail.polimi.it>
2023-08-05 23:15:27 +0200

    Minor changes

--- api_input.c
+++ api_input.c
@@ -93,27 +93,3 @@ int main()
 
     return 0;
 }
-
-/* aggiungi-stazione distanza numero-auto autonomia-auto-1 ... autonomia-auto-n
-Aggiunge una stazione, posta alla distanza indicata, dotata di numero-auto, dall’autonomia indicata.
-Ad esempio:
-aggiungi-stazione 10 3 100 200 300
-aggiunge una stazione a distanza 10 dall’inizio dell’autostrada, con un parco di tre veicoli con autonomia
-100, 200 e 300 km rispettivamente. Se esiste già una stazione alla distanza indicata, il comando non fa nulla.
-Stampa attesa come risposta: aggiunta oppure non aggiunta.
-• demolisci-stazione distanza
-Rimuove la stazione posta alla distanza indicata, se essa esiste.
-Stampa attesa come risposta: demolita oppure non demolita.
-• aggiungi-auto distanza-stazione autonomia-auto-da-aggiungere
-Se la stazione esiste, aggiunge un’auto alla stessa. È possibile avere più auto con la stessa autonomia.
-Stampa attesa come risposta: aggiunta oppure non aggiunta.
-• rottama-auto distanza-stazione autonomia-auto-da-rottamare
-Rimuove un’automobile dalla stazione indicata, se la stazione esiste ed è dotata di almeno un’automobile
-con l’autonomia indicata.
-Stampa attesa come risposta: rottamata oppure non rottamata.
-• pianifica-percorso distanza-stazione-partenza distanza-stazione-arrivo
-Richiede di pianificare il percorso con i vincoli sopra indicati.
-Stampa attesa come risposta: le tappe in ordine di percorrenza, rappresentate con la distanza delle stazioni
-da inizio autostrada, separate da spazi e alla fine seguite da un a-capo. Devono essere incluse partenza e
-arrivo; se coincidono la stazione viene stampata una sola volta. Se il percorso non esiste, stampa nessun
-percorso. L’azione di*/
